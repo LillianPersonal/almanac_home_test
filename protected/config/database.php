65659b39e2255754bf01b46cfd130440d9b67861
Lillian Hawasli <lillianhawasli@Lillians-MacBook-Pro.local>
2014-12-28 18:54:11 -0800

    initial commit

---
+++ protected/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	'connectionString' => 'mysql:port=8889;host=localhost;dbname=Testing',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'root',
+	'charset' => 'utf8',
+);
